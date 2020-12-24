951510608410b8d79efa263634f99eb2ee448420
Leon E <leon.eckerbom@gmail.com>
2020-12-25 00:24:32 +0100

    Add base functionality and comments regarding class members.

--- objectPractice/objectPractice/Program.cs
+++ objectPractice/objectPractice/Program.cs
@@ -7,6 +7,29 @@ namespace objectPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // This calls the object to make a new instance of it with the name newClass.
+            fibanacciEngine newClass = new fibanacciEngine();
+
+            // This sets the newClass instance's 'Base' property to 3.
+            newClass.Base = 3;
+
+            // This calls the 'AddWith' functionality with the int 4 and
+            // sets the new int 'found' to the returned value.
+            int found = newClass.AddWith(4);
+
+            // Prints result.
+            Console.WriteLine(found);
+
+
+            // This will now be repeated to show that it works for two instances.
+            fibanacciEngine secondClass = new fibanacciEngine();
+
+            secondClass.Base = 1;
+
+            int second = secondClass.AddWith(2);
+
+            Console.WriteLine(second);
         }
     }
 }
---
+++ objectPractice/objectPractice/fibanacciEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace objectPractice
+{
+	public class fibanacciEngine
+	{
+		// Property.
+		// This is a variable that can be set externally
+		// and used by this script to do something.
+		public int Base { get; set; }
+
+		// Method.
+		// This is a functionality that can be called externally
+		// and that will then do something.
+		// In this case it will return the base property with an int
+		// specified in the ...() on the external.
+
+		public int AddWith(int num)
+        {
+			return num + Base;
+        }
+
+		// Instance Constructor.
+		// This is the heart of the class. Once called it will create an
+		// instance of this using custom variables.
+		public fibanacciEngine()
+        {
+			Base = 0;
+        }
+	}
+}
